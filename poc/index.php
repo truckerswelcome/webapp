16100d617bc3f9c2d7f071675bfe49e07ef4e229
Jill San Luis <jill.devs@gmail.com>
2020-04-10 14:26:13 -0400

    Added some links to easily share the app to social media. See #15

--- poc/index.php
+++ poc/index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html >
 <head>
-   <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-   <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-   <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-   <script src="/jquery/jquery-3.4.1.min.js"></script>
-   <script src="/bootstrap/js/bootstrap.min.js"></script>
-   <title>Truckers Welcome</title>
-   <link rel="stylesheet" type="text/css" href="css/tw.css">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <script src="/jquery/jquery-3.4.1.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" integrity="undefined" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <title>Truckers Welcome</title>
+    <link rel="stylesheet" type="text/css" href="css/tw.css">
 </head>
 <body> 
    <aside class="sidenav">
@@ -22,6 +26,11 @@
          <li><a href="about.html">About</a></li>
          <li><a href="mailto:help@truckerswelcome.ca?subject=Mail from TruckersWelcome">Email Us</a></li>
          <li><a href="tos.html">Terms of Service</a></li>
+          <li>
+              <a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.truckerswelcome.ca/"><span class="fa fa-facebook"></span></a>
+              <a class="btn btn-social-icon btn-twitter" href="https://twitter.com/intent/tweet?text=Help%20us%20spread%20the%20word%20and%20help%20others%20keep%20safe!%20https%3A//www.truckerswelcome.ca/"><span class="fa fa-twitter"></span></a>
+              <a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.truckerswelcome.ca/&title=Help%20us%20spread%20the%20word%20and%20help%20others%20keep%20safe!&summary=&source="><span class="fa fa-linkedin"></span></a>
+          </li>
       </ul>
    </aside>
    <div class="overlay"></div>
